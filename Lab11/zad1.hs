7225604268858aefbcf5762dac359dafa5862ec3
Mikołaj Myśliński <122750466+MikolajMys@users.noreply.github.com>
2024-06-05 20:58:44 +0200

    Done zad1

--- Lab11/zad1.hs
+++ Lab11/zad1.hs
@@ -1,7 +1,13 @@
-addMe :: Integer -> Integer -> Integer
-addMe x y = x + y
+addMe1 :: Integer -> Integer -> Integer
+addMe1 x y = (+) x  y
+addMe2 :: Integer -> Integer -> Integer
+addMe2 x y = x + y
 
 main :: IO ()
-main =  do
+main = do
   putStr "Sum of x + y = "
-  print(addMe 10 25)
+  --infix
+  print(10 `addMe1` 25)
+  putStr "Sum of x + y = "
+  --prefix
+  print(addMe2 10 25)
